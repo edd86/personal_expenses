9053d8b19d89576fb4878f8e2dcb9b4a581b5447
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2025-11-17 20:28:38 -0400

    Fix enum color name formatting and remove unused color conversion methods

--- lib/core/constants/enum_color.dart
+++ lib/core/constants/enum_color.dart
@@ -5,22 +5,10 @@ enum EnumColors {
   plumb(Color(0xFF364954), 'Plomo'),
   silver(Color(0xFF99aebb), 'Silver'),
   pink(Color(0xFFdb91cd), 'Rosa'),
-  darkPink(Color(0xFFa35d96), 'Palo Rosa');
+  darkPink(Color(0xFFa35d96), 'Palo_Rosa');
 
   final Color color;
   final String name;
 
   const EnumColors(this.color, this.name);
-
-  // Obtener el color
-  static Color hexToColor(String hex) {
-    dynamic color = hex.toUpperCase().replaceAll('#', '');
-    if (color.length == 6) color = 'FF$color';
-    return Color(int.parse(color, radix: 16));
-  }
-
-  //Obtener el Hexadecimal
-  String toHex() {
-    return color.toARGB32().toRadixString(16).substring(2).toUpperCase();
-  }
 }
